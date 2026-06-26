407208c0163d3f6e531cb7e4ff46f5d352072606
jeetu yadav <jeetuyadav2091@gmail.com>
2026-06-27 00:40:25 +0530

    Update socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,21 +7,21 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173"]
+        origin: ["http://localhost:5173", "https://starlit-moxie-782d49.netlify.app"],
+        credentials: true,
     }
 });
 
 export function getReceiverSocketId(userId){
     return userSocketMap[userId];
 }
+
 const userSocketMap = {}; // {userId: socketId}
 
 io.on("connection", (socket) => {
     console.log("A user connected", socket.id);
-
     const userId = socket.handshake.query.userId
     if(userId) userSocketMap[userId] = socket.id
-
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
     socket.on("disconnect", () => {
