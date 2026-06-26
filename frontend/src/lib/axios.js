52b09bf7c9092da382dc2179ab3a7bfb8ff338e5
jeetu yadav <jeetuyadav2091@gmail.com>
2026-06-26 23:59:30 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
+
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development" 
     ? "http://localhost:5001/api" 
-    : "https://chat-application-r793.onrender.com",
+    : "https://chat-application-r793.onrender.com/api",   // ✅ /api add kiya
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
