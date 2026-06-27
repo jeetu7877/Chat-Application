3013f241697ea2fcfc5fcb74cedb897c57c56094
jeetu yadav <jeetuyadav2091@gmail.com>
2026-06-27 18:25:52 +0530

    Update auth.route.js

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,14 @@
 import express from "express";
-import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
+import {
+  checkAuth,
+  login,
+  logout,
+  signup,
+  updateProfile,
+  blockUser,
+  unblockUser,
+  getBlockedUsers,
+} from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
@@ -7,9 +16,12 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
-
 router.put("/update-profile", protectRoute, updateProfile);
-
 router.get("/check", protectRoute, checkAuth);
+
+// Block / Unblock
+router.post("/block/:id", protectRoute, blockUser);
+router.delete("/unblock/:id", protectRoute, unblockUser);
+router.get("/blocked-users", protectRoute, getBlockedUsers);
 
 export default router;
