4d029fc98a6cfac96f275ea86bc7897c1cc859cd
jeetu yadav <jeetuyadav2091@gmail.com>
2026-06-27 13:03:45 +0530

    Update message.route.js

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,13 +1,21 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getMessages, getUsersForSidebar, sendMessage, deleteMessage, editMessage } from "../controllers/message.controller.js";
+import { 
+  getMessages, 
+  getUsersForSidebar, 
+  sendMessage, 
+  deleteMessage, 
+  editMessage,
+  markAsRead
+} from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
 router.get("/:id", protectRoute, getMessages);
 router.post("/send/:id", protectRoute, sendMessage);
-router.delete("/:id", protectRoute, deleteMessage);  // ← Add
-router.put("/:id", protectRoute, editMessage);        // ← Add
+router.delete("/:id", protectRoute, deleteMessage);
+router.put("/:id", protectRoute, editMessage);
+router.put("/read/:id", protectRoute, markAsRead);  // ← Add
 
 export default router;
