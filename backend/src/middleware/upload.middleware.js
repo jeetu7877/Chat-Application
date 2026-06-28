05bc2606dd9033db6de59a8b866d5c87221e8381
jeetu yadav <jeetuyadav2091@gmail.com>
2026-06-28 18:17:46 +0530

    Create upload.middleware.js

---
+++ backend/src/middleware/upload.middleware.js
@@ -0,0 +1,21 @@
+import multer from "multer";
+
+const storage = multer.memoryStorage();
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image/") || file.mimetype.startsWith("video/")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Invalid file type. Only images and videos are allowed!"), false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+  limits: {
+    fileSize: 15 * 1024 * 1024,
+  },
+});
+
+export default upload;
